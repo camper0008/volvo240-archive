56b4099cc843986637e34f16382d157e7b5b0ffc
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2023-10-17 13:40:27 +0200

    update start script

--- host/start-docker.sh
+++ host/start-docker.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-docker run -ditp 8081:3333 --name old-volvo240 --restart always -v scraper.db:/workspace/scraper.db $1
+read -p "username: " USERNAME
+read -p "Password: " PASSWORD
+docker run -ditp 8081:3333 --name old-volvo240 -e "USERNAME=$USERNAME" -e "PASSWORD=$PASSWORD" --restart always -v ./scraper.db:/workspace/scraper.db $1

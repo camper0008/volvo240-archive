b3aaef007929fe82786f3afecb99536cfcfb3b9d
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2023-12-07 22:21:35 +0100

    host: small stylistic tweaks

--- host/assets/search.js
+++ host/assets/search.js
@@ -22,14 +22,14 @@ async function search(query, page, limit, key) {
     // }
     const index = "post";
     const api_url = "https://meili.volvo240.dk"
-    const url = `${api_url}/indexes/${index}/search?q=${query}&attributesToRetrieve=title,content,author,post_id,forum_id&attributesToHighlight=title,content&highlightPreTag=<span class="highlighted">&highlightPostTag=</span>&page=${page}&hitsPerPage=${limit}`;
+    const url = `${api_url}/indexes/${index}/search?q=${query}&attributesToRetrieve=title,content,author,post_id,date,forum_id&attributesToHighlight=title,content&highlightPreTag=<span class="highlighted">&highlightPostTag=</span>&page=${page}&hitsPerPage=${limit}`;
     const headers = new Headers({"Authorization": `Bearer ${key}`});
 
     return await (await fetch(url, { headers })).json();
 }
 
 async function onInput(query, key) {
-    const response = await search(query, 1, 20, key);
+    const response = await search(query, 1, 5, key);
     const results = document.getElementById("search-results");
     const new_results = response.hits
         .map((hit) => {
@@ -37,7 +37,7 @@ async function onInput(query, key) {
             card.classList.add("card");
             const title = document.createElement("p");
             title.innerHTML = hit._formatted.title
-            title.innerHTML = "<b>" + title.innerHTML + "</b>" + "<span class='author'> af " + hit.author + "</span>";
+            title.innerHTML = `<b>${title.innerHTML}</b> <span class='author'>af ${hit.author} d. ${hit.date}</span>`;
             const content = document.createElement("p");
             content.classList.add("text-post");
             content.innerHTML = hit._formatted.content;

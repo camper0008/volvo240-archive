6e8a1e8e53515d8a3376ba5c251daaf3f6a7d8e8
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2023-12-07 23:04:18 +0100

    host: update scripts

--- host/update-docker.sh
+++ host/update-docker.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+docker stop old-volvo240
+docker rm old-volvo240
+docker build -t $1 .
 docker run -ditp 8081:3333 --name old-volvo240 --restart always -v ./scraper.db:/workspace/scraper.db $1

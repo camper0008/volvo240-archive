adfb0ef9eb7f74bdf4565f4e1033a0938291376b
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2023-12-07 20:41:22 +0100

    auth header

--- host/upload_posts_list.sh
+++ host/upload_posts_list.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+## upload posts to meilisearch
+
 # extract posts as json
 sqlite3 scraper.db < ".mode json\n.once posts.json\n.SELECT * FROM post;"
+
+read -p "authorization key: "
 
 # create post index
 curl \
   -X POST 'http://localhost:7700/indexes' \
   -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer $REPLY' \
   --data-binary '{
     "uid": "post"
   }'
@@ -15,4 +20,5 @@ curl \
 curl \
   -X POST 'http://localhost:7700/indexes/post/documents' \
   -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer $REPLY' \
   --data-binary "$(cat posts.json)"

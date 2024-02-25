2249a5a4f421f40cc1977f5760e2ec903c050fd7
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2024-02-25 16:23:31 +0100

    file-explorer: link to archive

--- file-explorer/index.js
+++ file-explorer/index.js
@@ -23,7 +23,7 @@ async function main() {
         .then(entry => entry.map(([filename, url]) => [filename, new URL(url)]))
     app.get("/", (req, res) => {
         const list = app_list.map(([filename, url]) => `<a href="${url.pathname}${url.search}">${url.pathname}${url.search}</a>`)
-        res.send('<h1>Volvo240.dk</h1><hr><p><a href="/default.asp">Link til forsiden</a></p><hr><p>Her er en liste af alle filer fundet:</p> ' + list.join("<br>"));
+        res.send('<h1>Volvo240.dk</h1><hr><p><a href="/default.asp">Link til forsiden</a></p><p><a href="/arkiv/">Link til forum indlæg</a></p><hr><p>Her er en liste af alle ikke-forum filer fundet:</p> ' + list.join("<br>"));
     })
     app.get("/*", (req, res) => {
         const entry = app_list.find(([_, url]) => req.originalUrl === `${url.pathname}${url.search}`)
